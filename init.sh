daa14096f1c36ff95ef8b21d621846b58e640db3
Anna Zhurba <anna0zhurba@gmail.com>
2024-01-15 16:59:51 +0100

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MYSQL_HOST=db:3306
+MYSQL_USER=root
+MYSQL_PASSWORD=student
+DB_NAME=BE_186249
+DB_CONTAINER=BE_186249_prestashop_mysql
+
+docker exec -i "$DB_CONTAINER" mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" -e "use $DB_NAME;" 2>/dev/null 
+
+if [ $? != 0 ]; then
+    docker exec -i "$DB_CONTAINER" mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" -e "CREATE DATABASE $DB_NAME;"
+    docker exec -i "$DB_CONTAINER" mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" -D "$DB_NAME" < "mysql.sql"
+fi
+
+docker stack deploy -c docker-compose.yml BE_186249 --with-registry-auth
