9ea93afeade81548679ae621051e6aa4f075b626
Anna Zhurba <anna0zhurba@gmail.com>
2024-01-16 22:27:51 +0100

    Add files via upload

--- mysql.sql
+++ mysql.sql
@@ -19,9 +19,6 @@ SET time_zone = "+00:00";
 
 --
 -- Database: `prestashop`
---
-CREATE DATABASE IF NOT EXISTS `prestashop` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `prestashop`;
 
 -- --------------------------------------------------------
 
