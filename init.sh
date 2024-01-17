02d32db0a698115680721ba0e5ec30b02c2e2f86
Anna Zhurba <anna0zhurba@gmail.com>
2024-01-17 11:02:53 +0100

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,15 +1,67 @@
-#!/bin/bash
+#!/bin/sh
 
-MYSQL_HOST=db
-MYSQL_USER=root
-MYSQL_PASSWORD=student
-DB_NAME=BE_186249
-DB_CONTAINER=3fba02312d95
+if [ "$DB_SERVER" = "<to be defined>" -a $PS_INSTALL_AUTO = 1 ]; then
+    echo >&2 'error: You requested automatic PrestaShop installation but MySQL server address is not provided '
+    echo >&2 '  You need to specify DB_SERVER in order to proceed'
+    exit 1
+elif [ "$DB_SERVER" != "<to be defined>" -a $PS_INSTALL_AUTO = 1 ]; then
+    RET=1
+    while [ $RET -ne 0 ]; do
+        echo "\n* Checking if $DB_SERVER is available..."
+        mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "status" > /dev/null 2>&1
+        RET=$?
 
-docker exec -i "$DB_CONTAINER" mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" -e "use $DB_NAME;" 2>/dev/null 
+        if [ $RET -ne 0 ]; then
+            echo "\n* Waiting for confirmation of MySQL service startup";
+            sleep 5
+        fi
+    done
+        echo "\n* DB server $DB_SERVER is available, let's continue !"
+fi
 
-echo 'przed ifem'
-docker exec -i "$DB_CONTAINER" mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" -D "$DB_NAME" < "mysql.sql"
-echo 'po ifie'
+# From now, stop at error
+set -e
 
-docker stack deploy -c docker-compose.yml BE_186249 --with-registry-auth
+if [ ! -f ./config/settings.inc.php ]; then
+    rm ./config.settings.inc.php
+fi
+
+if [ $PS_INSTALL_AUTO = 1 ]; then
+
+    echo "\n* Installing PrestaShop, this may take a while ...";
+
+    if [ $PS_ERASE_DB = 1 ]; then
+        echo "\n* Drop & recreate mysql database...";
+        if [ $DB_PASSWD = "" ]; then
+            echo "\n* Dropping existing database $DB_NAME..."
+            mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -e "drop database if exists $DB_NAME;"
+            echo "\n* Creating database $DB_NAME..."
+            mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER create $DB_NAME --force;
+        else
+            echo "\n* Dropping existing database $DB_NAME..."
+            mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "drop database if exists $DB_NAME;"
+            echo "\n* Creating database $DB_NAME..."
+            mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD create $DB_NAME --force;
+            mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD < /tmp/dump/dump.sql #BAZA DUMP - JAK NIE DZIALA TO PRZED EXEC WYWALIC
+        fi
+    fi
+
+    if [ "$PS_DOMAIN" = "<to be defined>" ]; then
+        export PS_DOMAIN=$(hostname -i)
+    fi
+
+    echo "\n* Launching the installer script..."
+    runuser -g www-data -u www-data -- php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
+    --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
+    --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
+    --password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
+    --all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL
+
+    if [ $? -ne 0 ]; then
+        echo 'warning: PrestaShop installation failed.'
+    fi
+fi
+
+echo "\n* Almost ! Starting web server now\n";
+
+exec apache2-foreground
